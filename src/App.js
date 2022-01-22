c20b5a8e96a2e9bd2f87bda71d6243803f5c0a75
rexhsiao <f9281056@gmail.com>
2022-01-22 20:55:31 +1000

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React, { useState } from 'react';
 
 import HomePage from '../src/pages/homePage';
-import SearchPage from '../src/pages/searchPage';
-import DetailPage from '../src/pages/detailPage';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter } from "react-router-dom";
 import './App.css';
 
 const App = () => {
@@ -13,12 +11,9 @@ const App = () => {
     setToggle(!toggle);
   }
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<HomePage handleToggle={handleToggle} toggle={toggle}/>}/>
-      </Routes>
-    </BrowserRouter>
-    
+    <HashRouter base="/">
+      <HomePage handleToggle={handleToggle} toggle={toggle}/>
+    </HashRouter>
   );
 }
 
